cbab47e48dd0e2f4958909330dffcc59a2405728
Maxine Meijboom <MaxineCodes@users.noreply.github.com>
2026-04-09 14:28:19 +0200

    navbar, main page container and css setup

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static', template_folder='templates')
 
 
 @app.route("/")
+#def index():
+#    return render_template("index.html")
 def index():
-    return render_template("index.html")
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
