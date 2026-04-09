c351075fb74872b84e5f5ee61939fe108c46f19a
Maxine Meijboom <MaxineCodes@users.noreply.github.com>
2026-04-09 16:13:40 +0200

    added portfolio and resume pages

--- app.py
+++ app.py
@@ -4,6 +4,17 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 
 
 @app.route("/")
+@app.route("/")
+def index():
+    return render_template("index.html")
+@app.route("/portfolio")
+def portfolio():
+    return render_template("portfolio.html")
+@app.route("/resume")
+def resume():
+    return render_template("resume.html")
+
+
 #def index():
 #    return render_template("index.html")
 def index():
