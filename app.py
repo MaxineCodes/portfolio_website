d0a7fb917e1549013200b9875f6664d192cc730f
Maxine Meijboom <MaxineCodes@users.noreply.github.com>
2026-04-12 12:05:09 +0200

    added tech, writing, and blog routes and html files

--- app.py
+++ app.py
@@ -10,12 +10,21 @@ def index():
 @app.route("/portfolio")
 def portfolio():
     return render_template("portfolio.html")
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
 @app.route("/resume")
 def resume():
     return render_template("resume.html")
 @app.route("/contact")
 def contact():
     return render_template("contact.html")
+@app.route("/tech")
+def tech():
+    return render_template("tech.html")
+@app.route("/writing")
+def writing():
+    return render_template("writing.html")
 
 
 #def index():
